3aaf7a6197413201d579c2928ddf1f08fde18e72
joaolucaspcd <sebastiaodonizetimariano@gmail.com>
2026-02-11 15:05:39 -0300

    11/02

---
+++ aula 01/script.js
@@ -0,0 +1,21 @@
+var nome = "joao lucas";
+
+var idade = 17;
+
+var altura = 1.88;
+
+console.log( nome );
+
+console.log(idade);
+
+console.log(altura);
+
+document.write("<h1> ola, seja bem vindo ao javaScript</h1>")
+
+document.write(nome + "<br>");
+document.write(idade +"<br>");
+document.write(altura +"<br>");
+document.write("<p style='color: red'>Você já viveu " + dias + " dias</p>");
+
+
+           
